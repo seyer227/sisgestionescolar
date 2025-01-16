ca6d2af1fc07ae96d3c7e622b340ea1b5ec8b51c
ALBERTO REYES DENEGRI <151667703+seyer227@users.noreply.github.com>
2025-01-16 12:57:09 -0600

    configuracion de login

--- app/config.php
+++ app/config.php
@@ -9,11 +9,20 @@ define ('APP_NAME','SISTEMA DE GESTION ESCOLAR');
 define ('APP_URL','http://localhost/sisgestionescolar');
 define ('KEY_API_MAPS','');
 
-$servidor = "mysql:dbname=".DB.";host=".SERVIDOR;
+$servidor = "mysql:dbname=".BD.";host=".SERVIDOR;
 
 try {
-    $pdo = new PDO ($servidor, username:USUARIO ,passwd:PASSWORD,
-            array (PDO::MYSQL_))
-} catch (\Throwable $th) {
-    //throw $th;
+    $pdo = new PDO ($servidor, USUARIO, PASSWORD,
+            array (PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+            "conexion exitosa a la base de datos";
+} catch (PDOException $e) {
+    "conexion fallida a la base de datos";
 }
+date_default_timezone_set('America/Mexico_City');
+$fechahora = date(format: 'Y-m-d H:i:s');
+$fecha_actual= date(format: 'Y-m-d');
+$dia_actual = date(format: 'd');
+$mes_actual = date(format: 'm');
+$ano_actual = date(format: 'Y');
+$hora = date(format: 'H:i:s');
+
--- index.php
+++ index.php
@@ -1,3 +1,23 @@
 <?php
+include('app/config.php');
+include('layout/parte1.php');
+?>
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Main content -->
+     <br>
+    <div class="content">
+      <div class="container-fluid">
+        <div class="row">
+          <h1>Vista Principal</h1>
+        </div>
+        <!-- /.row -->
+      </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.content -->
+  </div>
+  <!-- /.content-wrapper -->
 
-echo "hola mundo";
+<?php
+include('layout/parte2.php');
+?>
