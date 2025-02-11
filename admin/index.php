28d26e3a34184b6508d0eb61fb09eab121940b73
ALBERTO REYES DENEGRI <151667703+seyer227@users.noreply.github.com>
2025-02-11 12:31:46 -0600

    Usuarios y vista previa modificados

--- admin/index.php
+++ admin/index.php
@@ -1,6 +1,8 @@
 <?php
 include('../app/config.php');
 include('../admin/layout/parte1.php');
+include('../app/controllers/roles/listado_de_roles.php');
+include('../app/controllers/usuarios/listado_de_usuarios.php');
 ?>
 <!-- Content Wrapper. Contains page content -->
 <div class="content-wrapper">
@@ -9,7 +11,52 @@ include('../admin/layout/parte1.php');
   <div class="content">
     <div class="container-fluid">
       <div class="row">
-        <h1>Vista Principal</h1>
+        <h1><?= APP_NAME ?></h1>
+      </div>
+      <br>
+      <div class="row">
+        <div class="col-lg-3 col-6">
+          <!-- small card -->
+          <div class="small-box bg-primary">
+            <div class="inner">
+              <?php
+              $contador_roles = 0;
+              foreach ($roles as $rol) {
+                $contador_roles++;
+              }
+              ?>
+              <h3><?=$contador_roles?></h3>
+              <p>Roles registrados</p>
+            </div>
+            <div class="icon">
+              <i class="fas"><i class="bi bi-person-fill-gear"></i></i>
+            </div>
+            <a href="<?=APP_URL;?>/admin/roles" class="small-box-footer">
+              Más información <i class="fas fa-arrow-circle-right"></i>
+            </a>
+          </div>
+        </div>
+        <div class="col-lg-3 col-6">
+            <!-- small card -->
+            <div class="small-box bg-info">
+              <div class="inner">
+                <?php
+                $contador_usuarios = 0;
+                foreach ($usuarios as $usuario) {
+                  $contador_usuarios++;
+                }
+                ?>
+                <h3><?=$contador_usuarios?></h3>
+                <p>Usuarios registrados</p>
+              </div>
+              <div class="icon">
+                <i class="fas fa-user-plus"></i>
+              </div>
+              <a href="<?=APP_URL;?>/admin/usuarios" class="small-box-footer">
+                Más Información <i class="fas fa-arrow-circle-right"></i>
+              </a>
+            </div>
+          </div>
       </div>
       <!-- /.row -->
     </div><!-- /.container-fluid -->
